d517a27579c85e6da1d005c688dc7a83bbcdf3eb
Lamond Lu <Lamond Lu>
2017-03-22 03:38:21 -0400

    Change the start log

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('./config/express'),
-    config = require('./config/config')
-db = require('./config/mongodb');
+    config = require('./config/config'),
+    db = require('./config/mongodb');
 
 var db = db();
-var config = config();
+var serverPort = config.serverPort;
 var app = express(db);
 
-app.listen(config.serverPort);
+app.listen(serverPort);
 module.exports = app;
 
-console.log('Server running at http://localhost:3000');
+console.log('Server running at http://localhost:' + serverPort);
